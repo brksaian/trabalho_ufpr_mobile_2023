6b5349704139b89ba62efb3ee12c78de1f0b779a
brksaian <braianviana@hotmail.com>
2023-11-01 16:21:02 -0300

    Padronização na forma de importação do token da API da TMDB

--- Back/src/routes/Filmes/listarFilmePagina.ts
+++ Back/src/routes/Filmes/listarFilmePagina.ts
@@ -2,6 +2,7 @@ import { FastifyInstance, FastifyRequest } from "fastify";
 import { prisma } from "../../lib/prisma";
 import { z } from "zod";
 const fetch = require("node-fetch");
+require("dotenv").config();
 
 const paramsSchema = z.object({
   page: z.string(),
@@ -14,14 +15,13 @@ export async function listarFilmesPagina(app: FastifyInstance) {
       const params = paramsSchema.parse(request.params);
 
       const page = params.page;
-
+      const apiToken = process.env.TOKEN_MOVIEDB;
       const url = `https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=true&language=pt-BR&page=${page}&sort_by=popularity.desc`;
       const options = {
         method: "GET",
         headers: {
           accept: "application/json",
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiM2Y2ZmE3MzMxNzY5NTAyNmFjNTM1YzM1YzA5NGMyYyIsInN1YiI6IjY0ZmY1ODJjZWIxNGZhMDEwMGU2ZTZiOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j6koGbHeb2SMdF_TV0xeUNxXFasIwleUEKJ76NLP36w",
+          Authorization: `Bearer ${apiToken}`,
         },
       };
 
--- Back/src/routes/Filmes/listarGeneros.ts
+++ Back/src/routes/Filmes/listarGeneros.ts
@@ -6,12 +6,12 @@ const fetch = require("node-fetch");
 export async function listarTodosGeneros(app: FastifyInstance) {
   app.get("/filme/listar/genero", async (request: FastifyRequest, reply) => {
     const url = "https://api.themoviedb.org/3/genre/movie/list?language=pt-BR";
+    const apiToken = process.env.TOKEN_MOVIEDB;
     const options = {
       method: "GET",
       headers: {
         accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiM2Y2ZmE3MzMxNzY5NTAyNmFjNTM1YzM1YzA5NGMyYyIsInN1YiI6IjY0ZmY1ODJjZWIxNGZhMDEwMGU2ZTZiOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j6koGbHeb2SMdF_TV0xeUNxXFasIwleUEKJ76NLP36w",
+        Authorization: `Bearer ${apiToken}`,
       },
     };
 
